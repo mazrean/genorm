340b08285b2cb44e0efc04ed8e671dfce3f2b539
mazrean <shunsuke.w04@gmail.com>
2021-02-14 22:04:37 +0900

    controllerのdomainを用いての書き換え

--- cmd/controller/models/root.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// Root yamlのルート
-type Root struct {
-	Config *Config  `yaml:"config"`
-	Tables []*Table `yaml:"tables"`
-}
---
+++ cmd/controller/yaml_model/column.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/mazrean/gopendb-generator/cmd/domain"
+
+// Column カラムの構造体(yaml用)
+type Column struct {
+	domain.Column `yaml:",inline"`
+	Reference     []*Reference  `yaml:"reference"`
+	Extra         *domain.Extra `yaml:"extra"`
+}
