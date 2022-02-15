b7cd4eb29a38cf7f2d229fee2237aaaef68ef438
mazrean <shunsuke.w04@gmail.com>
2022-02-15 23:17:36 +0900

    Context作成

---
+++ context.go
@@ -0,0 +1,28 @@
+package genorm
+
+type Context[T TableBase] struct {
+	table  T
+	errs   []error
+}
+
+func newContext[T TableBase](table T) *Context[T] {
+	return &Context[T]{
+		table: table,
+	}
+}
+
+func (c *Context[T]) Table() T {
+	return c.table
+}
+
+func (c *Context[T]) addError(err error) {
+	c.errs = append(c.errs, err)
+}
+
+func (c *Context[T]) Errors() []error {
+	if len(c.errs) == 0 {
+		return nil
+	}
+
+	return c.errs
+}
