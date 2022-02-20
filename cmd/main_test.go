adfd28464dc7b04492328a05b59b2165072ff9da
mazrean <shunsuke.w04@gmail.com>
2022-02-20 11:53:25 +0900

    TestDestinationDir

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -130,3 +130,96 @@ func main() {
 		})
 	}
 }
+
+func TestDestinationDir(t *testing.T) {
+	err := os.MkdirAll("./test", os.ModePerm)
+	if err != nil {
+		t.Fatalf("create test directory: %s", err)
+	}
+	defer func() {
+		err := os.RemoveAll("./test")
+		if err != nil {
+			t.Errorf("failed to remove test directory: %s", err)
+		}
+	}()
+
+	absPath, err := filepath.Abs("./test2")
+	if err != nil {
+		t.Fatalf("failed to get absolute path: %s", err)
+	}
+
+	tests := []struct {
+		description string
+		path        string
+		exists      bool
+		err         bool
+	}{
+		{
+			description: "empty destination -> error",
+			path:        "",
+			err:         true,
+		},
+		{
+			description: "normal destination -> success",
+			path:        "./test2",
+			exists:      true,
+		},
+		{
+			description: "non-existent destination -> success",
+			path:        "./test2",
+		},
+		{
+			description: "destination in directory -> success",
+			path:        "./test/test",
+			exists:      true,
+		},
+		{
+			description: "non-existent destination in directory -> success",
+			path:        "./test/test",
+		},
+		{
+			description: "destination(absolute path) -> success",
+			path:        absPath,
+			exists:      true,
+		},
+		{
+			description: "non-existent destination(absolute path) -> success",
+			path:        absPath,
+			exists:      true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			if test.exists {
+				err := os.MkdirAll(test.path, os.ModePerm)
+				if err != nil {
+					t.Fatalf("failed to create test file: %s", err)
+				}
+
+				defer func() {
+					err := os.RemoveAll(test.path)
+					if err != nil {
+						t.Errorf("failed to remove test file: %s", err)
+					}
+				}()
+			}
+
+			dest, err := destinationDir(test.path)
+			if err != nil {
+				if !test.err {
+					t.Fatalf("unexpected error: %s", err)
+				}
+				return
+			}
+
+			if test.err {
+				t.Fatalf("expected error but got none")
+			}
+
+			if dest != test.path {
+				t.Fatalf("unexpected destination: %s", dest)
+			}
+		})
+	}
+}
