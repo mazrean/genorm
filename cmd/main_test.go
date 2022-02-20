f3d0bb9b14a4aacc5989c8054a9a5f6c1e837083
mazrean <shunsuke.w04@gmail.com>
2022-02-20 11:44:26 +0900

    TestOpenSource

---
+++ cmd/main_test.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestOpenSource(t *testing.T) {
+	err := os.MkdirAll("./test", os.ModePerm)
+	if err != nil {
+		t.Fatalf("create test directory: %s", err)
+	}
+	defer func() {
+		err := os.RemoveAll("./test")
+		if err != nil {
+			t.Errorf("failed to remove test directory: %s", err)
+		}
+	}()
+
+	absPath, err := filepath.Abs("./test.go")
+	if err != nil {
+		t.Fatalf("failed to get absolute path: %s", err)
+	}
+
+	tests := []struct {
+		description string
+		path        string
+		exists      bool
+		content     string
+		err         bool
+	}{
+		{
+			description: "normal source -> success",
+			path:        "./test.go",
+			exists:      true,
+			content: `package main
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+`,
+		},
+		{
+			description: "source in directory -> success",
+			path:        "./test/main.go",
+			exists:      true,
+			content: `package main
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+`,
+		},
+		{
+			description: "source(absolute path) -> success",
+			path:        absPath,
+			exists:      true,
+			content: `package main
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+`,
+		},
+		{
+			description: "non-existent source -> error",
+			path:        "./test/main.go",
+			err:         true,
+		},
+		{
+			description: "empty source -> error",
+			path:        "",
+			err:         true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			if test.exists {
+				func() {
+					f, err := os.Create(test.path)
+					if err != nil {
+						t.Fatalf("failed to create test file: %s", err)
+					}
+					defer f.Close()
+
+					_, err = f.WriteString(test.content)
+					if err != nil {
+						t.Fatalf("failed to write test file: %s", err)
+					}
+				}()
+
+				defer func() {
+					err := os.Remove(test.path)
+					if err != nil {
+						t.Errorf("failed to remove test file: %s", err)
+					}
+				}()
+			}
+
+			src, err := openSource(test.path)
+			if err != nil {
+				if !test.err {
+					t.Fatalf("unexpected error: %s", err)
+				}
+				return
+			}
+			defer src.Close()
+
+			if test.err {
+				t.Fatalf("expected error but got none")
+			}
+
+			if test.exists {
+				sb := strings.Builder{}
+
+				_, err := io.Copy(&sb, src)
+				if err != nil {
+					t.Fatalf("failed to read source: %s", err)
+				}
+
+				actualContent := sb.String()
+				if actualContent != test.content {
+					t.Fatalf("unexpected content: %s", actualContent)
+				}
+			}
+		})
+	}
+}
