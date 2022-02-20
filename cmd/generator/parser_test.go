e1adc1bbe50f74f4a5b8bf11b4515e4e8685f11d
mazrean <shunsuke.w04@gmail.com>
2022-02-20 20:09:26 +0900

    TestParseGenDecl

--- cmd/generator/parser_test.go
+++ cmd/generator/parser_test.go
@@ -132,3 +132,129 @@ func TestParseFuncDecl(t *testing.T) {
 		})
 	}
 }
+
+func TestParseGenDecl(t *testing.T) {
+	t.Parallel()
+
+	fieldType := ast.NewIdent("string")
+
+	tests := []struct {
+		description string
+		g           *ast.GenDecl
+		tables      []*ParserTable
+		err         bool
+	}{
+		{
+			description: "normal gen decl -> success",
+			g: &ast.GenDecl{
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: ast.NewIdent("a"),
+						Type: &ast.StructType{
+							Fields: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Names: []*ast.Ident{
+											ast.NewIdent("s"),
+										},
+										Type: fieldType,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			tables: []*ParserTable{
+				{
+					StructName: "a",
+					Columns: []*ParserColumn{
+						{
+							Name:      "s",
+							FieldName: "s",
+							Type:      fieldType,
+						},
+					},
+					RefTables: []*ParserRefTable{},
+				},
+			},
+		},
+		{
+			description: "skip non-type spec",
+			g: &ast.GenDecl{
+				Specs: []ast.Spec{
+					&ast.ValueSpec{
+						Names: []*ast.Ident{
+							ast.NewIdent("a"),
+						},
+						Values: []ast.Expr{
+							ast.NewIdent("b"),
+						},
+						Type: fieldType,
+					},
+				},
+			},
+			tables: []*ParserTable{},
+		},
+		{
+			description: "skip non-struct type spec",
+			g: &ast.GenDecl{
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: ast.NewIdent("a"),
+						Type: &ast.ChanType{},
+					},
+				},
+			},
+			tables: []*ParserTable{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			tables, err := parseGenDecl(test.g)
+			if err != nil {
+				if !test.err {
+					t.Fatalf("failed to parse gen decl: %s", err)
+				}
+				return
+			}
+
+			if test.err {
+				t.Fatalf("expected error but got no error")
+			}
+
+			if len(tables) != len(test.tables) {
+				t.Fatalf("table length is not match")
+			}
+
+			for i, table := range tables {
+				if table.StructName != test.tables[i].StructName {
+					t.Fatalf("struct name is not match")
+				}
+
+				if len(table.Columns) != len(test.tables[i].Columns) {
+					t.Fatalf("column length is not match")
+				}
+
+				for j, column := range table.Columns {
+					if column.Name != test.tables[i].Columns[j].Name {
+						t.Fatalf("column name is not match")
+					}
+
+					if column.FieldName != test.tables[i].Columns[j].FieldName {
+						t.Fatalf("column field name is not match")
+					}
+
+					if column.Type != test.tables[i].Columns[j].Type {
+						t.Fatalf("column type is not match")
+					}
+				}
+
+				if len(table.RefTables) != len(test.tables[i].RefTables) {
+					t.Fatalf("ref table length is not match")
+				}
+			}
+		})
+	}
+}
