98b21bb4c7b0871c73de41a821a6827b2f728dc7
mazrean <shunsuke.w04@gmail.com>
2022-02-23 16:41:49 +0900

    impl Method Set StructName

--- cmd/generator/types.go
+++ cmd/generator/types.go
@@ -1,6 +1,9 @@
 package generator
 
-import "go/ast"
+import (
+	"errors"
+	"go/ast"
+)
 
 type Table struct {
 	StructName      string
@@ -13,6 +16,41 @@ type Table struct {
 type Method struct {
 	Type methodType
 	Decl *ast.FuncDecl
+}
+
+func (m *Method) SetStructName(structName string) error {
+	if m.Decl == nil ||
+		m.Decl.Recv == nil ||
+		len(m.Decl.Recv.List) == 0 ||
+		m.Decl.Recv.List[0] == nil ||
+		m.Decl.Recv.List[0].Type == nil {
+		return errors.New("invalid method")
+	}
+	switch m.Type {
+	case methodTypeIdentifier:
+		ident, ok := m.Decl.Recv.List[0].Type.(*ast.Ident)
+		if !ok || ident == nil {
+			return errors.New("invalid method")
+		}
+
+		ident.Name = structName
+	case methodTypeStar:
+		star, ok := m.Decl.Recv.List[0].Type.(*ast.StarExpr)
+		if !ok || star == nil {
+			return errors.New("invalid method")
+		}
+
+		ident, ok := star.X.(*ast.Ident)
+		if !ok || ident == nil {
+			return errors.New("invalid method")
+		}
+
+		ident.Name = structName
+	default:
+		return errors.New("unknown method type")
+	}
+
+	return nil
 }
 
 type JoinedTable struct {
