31535fac3d24943eb30786d649602dcc91902830
mazrean <shunsuke.w04@gmail.com>
2022-02-28 00:00:54 +0900

    fix types

--- cmd/generator/types/types.go
+++ cmd/generator/types/types.go
@@ -1,7 +1,6 @@
 package types
 
 import (
-	"errors"
 	"go/ast"
 )
 
@@ -16,41 +15,6 @@ type Table struct {
 type Method struct {
 	Type MethodType
 	Decl *ast.FuncDecl
-}
-
-func (m *Method) SetStructName(structName string) error {
-	if m.Decl == nil ||
-		m.Decl.Recv == nil ||
-		len(m.Decl.Recv.List) == 0 ||
-		m.Decl.Recv.List[0] == nil ||
-		m.Decl.Recv.List[0].Type == nil {
-		return errors.New("invalid method")
-	}
-	switch m.Type {
-	case MethodTypeIdentifier:
-		ident, ok := m.Decl.Recv.List[0].Type.(*ast.Ident)
-		if !ok || ident == nil {
-			return errors.New("invalid method")
-		}
-
-		ident.Name = structName
-	case MethodTypeStar:
-		star, ok := m.Decl.Recv.List[0].Type.(*ast.StarExpr)
-		if !ok || star == nil {
-			return errors.New("invalid method")
-		}
-
-		ident, ok := star.X.(*ast.Ident)
-		if !ok || ident == nil {
-			return errors.New("invalid method")
-		}
-
-		ident.Name = structName
-	default:
-		return errors.New("unknown method type")
-	}
-
-	return nil
 }
 
 type JoinedTable struct {
