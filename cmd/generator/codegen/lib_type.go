5aa2ebe987124b8dea0b875571d09d86fc4e47e4
mazrean <shunsuke.w04@gmail.com>
2022-02-28 00:04:17 +0900

    add types

--- cmd/generator/codegen/lib_type.go
+++ cmd/generator/codegen/lib_type.go
@@ -13,15 +13,25 @@ var (
 		X:   genormIdent,
 		Sel: ast.NewIdent("ExprType"),
 	}
+	columnFieldExprTypeExpr = &ast.SelectorExpr{
+		X:   genormIdent,
+		Sel: ast.NewIdent("ColumnFieldExpr"),
+	}
+	basicTableTypeExpr = &ast.SelectorExpr{
+		X:   genormIdent,
+		Sel: ast.NewIdent("BasicTable"),
+	}
 
 	exprExprIdent           = ast.NewIdent("Expr")
 	tableExprTableExprIdent = ast.NewIdent("TableExpr")
 	typedExprTypedExprIdent = ast.NewIdent("TypedExpr")
 
-	tableColumnsIdent        = ast.NewIdent("Columns")
-	tableGetErrors           = ast.NewIdent("GetErrors")
-	tableColumnMapIdent      = ast.NewIdent("ColumnMap")
-	basicTableTableNameIdent = ast.NewIdent("TableName")
+	tableColumnsIdent          = ast.NewIdent("Columns")
+	tableGetErrorsIdent        = ast.NewIdent("GetErrors")
+	tableAddErrorIdent         = ast.NewIdent("AddError")
+	tableColumnMapIdent        = ast.NewIdent("ColumnMap")
+	basicTableTableNameIdent   = ast.NewIdent("TableName")
+	joinedTableBaseTablesIdent = ast.NewIdent("BaseTables")
 
 	columnSQLColumnsIdent = ast.NewIdent("SQLColumnName")
 	columnTableNameIdent  = ast.NewIdent("TableName")
@@ -35,5 +45,12 @@ func wrappedPrimitive(primitive ast.Expr) ast.Expr {
 			Sel: ast.NewIdent("WrappedPrimitive"),
 		},
 		Index: primitive,
+	}
+}
+
+func typedTableColumn(tableType ast.Expr, exprType ast.Expr) ast.Expr {
+	return &ast.SelectorExpr{
+		X:   genormIdent,
+		Sel: ast.NewIdent("TypedTableColumn"),
 	}
 }
