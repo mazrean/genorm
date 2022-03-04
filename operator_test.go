576e17e507b91926989cebf224997f7691665d35
mazrean <shunsuke.w04@gmail.com>
2022-03-04 20:16:05 +0900

    TestIsNotNull

--- operator_test.go
+++ operator_test.go
@@ -1475,3 +1475,81 @@ func TestIsNull(t *testing.T) {
 		})
 	}
 }
+
+func TestIsNotNull(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		description   string
+		exprIsNil     bool
+		exprQuery     string
+		exprArgs      []genorm.ExprType
+		exprErrs      []error
+		expectedQuery string
+		expectedArgs  []genorm.ExprType
+		isError       bool
+	}{
+		{
+			description:   "normal",
+			exprQuery:     "(`hoge`.`huga` = ?)",
+			exprArgs:      []genorm.ExprType{genorm.Wrap(1)},
+			expectedQuery: "((`hoge`.`huga` = ?) IS NOT NULL)",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(1)},
+		},
+		{
+			description: "nil expr",
+			exprIsNil:   true,
+			isError:     true,
+		},
+		{
+			description: "expr error",
+			exprErrs:    []error{errors.New("expr1 error")},
+			isError:     true,
+		},
+		{
+			description:   "expr1 no args",
+			exprQuery:     "(`hoge`.`huga` = `hoge`.`huga`)",
+			exprArgs:      nil,
+			expectedQuery: "((`hoge`.`huga` = `hoge`.`huga`) IS NOT NULL)",
+			expectedArgs:  nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			var expr genorm.TypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]]
+			if test.exprIsNil {
+				expr = nil
+			} else {
+				mockExpr := mock.NewMockTypedTableColumn[*mock.MockTable, genorm.WrappedPrimitive[bool]](ctrl)
+				expr = mockExpr
+
+				mockExpr.
+					EXPECT().
+					Expr().
+					Return(test.exprQuery, test.exprArgs, test.exprErrs)
+			}
+
+			res := genorm.IsNotNull(expr)
+
+			assert.NotNil(t, res)
+
+			query, args, errs := res.Expr()
+			if test.isError {
+				assert.NotNil(t, errs)
+				assert.NotEmpty(t, errs)
+
+				return
+			}
+
+			if !assert.Nil(t, errs) {
+				return
+			}
+
+			assert.Equal(t, test.expectedQuery, query)
+			assert.Equal(t, test.expectedArgs, args)
+		})
+	}
+}
