e612efadd48f7ccd65f08009cbc5238e9cd55577
mazrean <shunsuke.w04@gmail.com>
2022-03-04 20:13:09 +0900

    TestGt

--- operator_test.go
+++ operator_test.go
@@ -1267,3 +1267,133 @@ func TestLt(t *testing.T) {
 		})
 	}
 }
+
+func TestGt(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		description   string
+		expr1IsNil    bool
+		expr1Query    string
+		expr1Args     []genorm.ExprType
+		expr1Errs     []error
+		expr2IsNil    bool
+		expr2Query    string
+		expr2Args     []genorm.ExprType
+		expr2Errs     []error
+		expectedQuery string
+		expectedArgs  []genorm.ExprType
+		isError       bool
+	}{
+		{
+			description:   "normal",
+			expr1Query:    "(`hoge`.`huga` = ?)",
+			expr1Args:     []genorm.ExprType{genorm.Wrap(1)},
+			expr2Query:    "(`hoge`.`huga` > ?)",
+			expr2Args:     []genorm.ExprType{genorm.Wrap(2)},
+			expectedQuery: "((`hoge`.`huga` = ?) > (`hoge`.`huga` > ?))",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(1), genorm.Wrap(2)},
+		},
+		{
+			description: "nil expr1",
+			expr1IsNil:  true,
+			expr2Query:  "(`hoge`.`huga` > ?)",
+			expr2Args:   []genorm.ExprType{genorm.Wrap(2)},
+			isError:     true,
+		},
+		{
+			description: "nil expr2",
+			expr1Query:  "(`hoge`.`huga` = ?)",
+			expr1Args:   []genorm.ExprType{genorm.Wrap(1)},
+			expr2IsNil:  true,
+			isError:     true,
+		},
+		{
+			description: "expr1 error",
+			expr1Errs:   []error{errors.New("expr1 error")},
+			expr2Query:  "(`hoge`.`huga` > ?)",
+			expr2Args:   []genorm.ExprType{genorm.Wrap(2)},
+			isError:     true,
+		},
+		{
+			description: "expr2 error",
+			expr1Query:  "(`hoge`.`huga` = ?)",
+			expr1Args:   []genorm.ExprType{genorm.Wrap(1)},
+			expr2Errs:   []error{errors.New("expr2 error")},
+			isError:     true,
+		},
+		{
+			description:   "expr1 no args",
+			expr1Query:    "(`hoge`.`huga` = `hoge`.`huga`)",
+			expr1Args:     nil,
+			expr2Query:    "(`hoge`.`huga` > ?)",
+			expr2Args:     []genorm.ExprType{genorm.Wrap(2)},
+			expectedQuery: "((`hoge`.`huga` = `hoge`.`huga`) > (`hoge`.`huga` > ?))",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(2)},
+		},
+		{
+			description:   "expr2 no args",
+			expr1Query:    "(`hoge`.`huga` = ?)",
+			expr1Args:     []genorm.ExprType{genorm.Wrap(1)},
+			expr2Query:    "(`hoge`.`huga` = `hoge`.`huga`)",
+			expr2Args:     nil,
+			expectedQuery: "((`hoge`.`huga` = ?) > (`hoge`.`huga` = `hoge`.`huga`))",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(1)},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			var expr1 genorm.TypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]]
+			if test.expr1IsNil {
+				expr1 = nil
+			} else {
+				mockExpr1 := mock.NewMockTypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]](ctrl)
+				expr1 = mockExpr1
+
+				if !test.expr2IsNil {
+					mockExpr1.
+						EXPECT().
+						Expr().
+						Return(test.expr1Query, test.expr1Args, test.expr1Errs)
+				}
+			}
+
+			var expr2 genorm.TypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]]
+			if test.expr2IsNil {
+				expr2 = nil
+			} else {
+				mockExpr2 := mock.NewMockTypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]](ctrl)
+				expr2 = mockExpr2
+
+				if !test.expr1IsNil {
+					mockExpr2.
+						EXPECT().
+						Expr().
+						Return(test.expr2Query, test.expr2Args, test.expr2Errs)
+				}
+			}
+
+			res := genorm.Gt(expr1, expr2)
+
+			assert.NotNil(t, res)
+
+			query, args, errs := res.Expr()
+			if test.isError {
+				assert.NotNil(t, errs)
+				assert.NotEmpty(t, errs)
+
+				return
+			}
+
+			if !assert.Nil(t, errs) {
+				return
+			}
+
+			assert.Equal(t, test.expectedQuery, query)
+			assert.Equal(t, test.expectedArgs, args)
+		})
+	}
+}
