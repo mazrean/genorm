a4c953214f4aec78bc03f5c297c9aea62e81eb19
mazrean <shunsuke.w04@gmail.com>
2022-03-09 17:37:23 +0900

    TestCount

--- function_test.go
+++ function_test.go
@@ -96,3 +96,90 @@ func TestAvg(t *testing.T) {
 		})
 	}
 }
+
+func TestCount(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		description   string
+		exprIsNil     bool
+		exprQuery     string
+		exprArgs      []genorm.ExprType
+		exprErrs      []error
+		distinct      bool
+		expectedQuery string
+		expectedArgs  []genorm.ExprType
+		isError       bool
+	}{
+		{
+			description:   "normal",
+			exprQuery:     "(`hoge`.`huga` = ?)",
+			exprArgs:      []genorm.ExprType{genorm.Wrap(1)},
+			expectedQuery: "COUNT((`hoge`.`huga` = ?))",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(1)},
+		},
+		{
+			description:   "distinct",
+			exprQuery:     "(`hoge`.`huga` = ?)",
+			exprArgs:      []genorm.ExprType{genorm.Wrap(1)},
+			distinct:      true,
+			expectedQuery: "COUNT(DISTINCT (`hoge`.`huga` = ?))",
+			expectedArgs:  []genorm.ExprType{genorm.Wrap(1)},
+		},
+		{
+			description: "nil expr",
+			exprIsNil:   true,
+			isError:     true,
+		},
+		{
+			description: "expr error",
+			exprErrs:    []error{errors.New("expr1 error")},
+			isError:     true,
+		},
+		{
+			description:   "expr1 no args",
+			exprQuery:     "(`hoge`.`huga` = `hoge`.`huga`)",
+			exprArgs:      nil,
+			expectedQuery: "COUNT((`hoge`.`huga` = `hoge`.`huga`))",
+			expectedArgs:  nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			var expr genorm.TypedTableExpr[*mock.MockTable, genorm.WrappedPrimitive[bool]]
+			if test.exprIsNil {
+				expr = nil
+			} else {
+				mockExpr := mock.NewMockTypedTableColumn[*mock.MockTable, genorm.WrappedPrimitive[bool]](ctrl)
+				expr = mockExpr
+
+				mockExpr.
+					EXPECT().
+					Expr().
+					Return(test.exprQuery, test.exprArgs, test.exprErrs)
+			}
+
+			res := genorm.Count(expr, test.distinct)
+
+			assert.NotNil(t, res)
+
+			query, args, errs := res.Expr()
+			if test.isError {
+				assert.NotNil(t, errs)
+				assert.NotEmpty(t, errs)
+
+				return
+			}
+
+			if !assert.Nil(t, errs) {
+				return
+			}
+
+			assert.Equal(t, test.expectedQuery, query)
+			assert.Equal(t, test.expectedArgs, args)
+		})
+	}
+}
